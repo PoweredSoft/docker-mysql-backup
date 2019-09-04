c65e9d915ec73dc6f9355fd1481a9f559511438b
David Lebee <david@poweredsoft.com>
2019-09-04 12:35:06 -0500

    Add project files.

---
+++ docker-mysql-backup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace docker_mysql_backup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
